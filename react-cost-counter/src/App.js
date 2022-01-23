a4911bd5fd33d37769add60a477a7096d9c94b82
Alex <olivesargreek@gmail.com>
2022-01-23 03:47:57 +0300

    create: props CostItem

--- react-cost-counter/src/App.js
+++ react-cost-counter/src/App.js
@@ -1,10 +1,43 @@
 import CostItem from "./components/CostItem";
 
 function App() {
+
+    const costs = [
+      {
+        date: new Date(2021, 2, 12),
+        description: 'Холодильник',
+        amount: 999.99
+      },
+      {
+        date: new Date(2021, 11, 25),
+        description: 'MacBook',
+        amount: 1259.43
+      },
+      {
+        date: new Date(2021, 2, 25),
+        description: 'Труселя',
+        amount: 29.56
+      }
+    ];
+
   return (
     <div>
       <h1>Start React App</h1>
-      <CostItem/>
+      <CostItem 
+      date={ costs[0].date }
+      description={ costs[0].description }
+      amount={ costs[0].amount }
+      />
+       <CostItem 
+      date={ costs[1].date }
+      description={ costs[1].description }
+      amount={ costs[1].amount }
+      />
+       <CostItem 
+      date={ costs[2].date }
+      description={ costs[2].description }
+      amount={ costs[2].amount }
+      />
     </div>
   );
 }
--- react-cost-counter/src/components/CostItem.js
+++ react-cost-counter/src/components/CostItem.js
@@ -1,12 +1,15 @@
 import './CostItem.css';
 
-function CostItem() {
+function CostItem(props) {
+    const costDate = new Date(2021, 2, 12);
+    const costDescription = 'Холодильник';
+    const costAmount = 999.99;
     return (
         <div className='cost-item'>
-            <div>Date</div>
+            <div>{ props.date.toISOString() }</div>
             <div className='cost-item__description'>
-                <h2>Element Counter</h2>
-                <div className='cost-item__price'>Sum</div>
+                <h2>{ props.description }</h2>
+                <div className='cost-item__price'>${ props.amount }</div>
             </div>   
         </div> 
     );
