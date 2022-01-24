b465f6d66f90a2ecfc1f413c22d7ac946d8fa990
Alex <olivesargreek@gmail.com>
2022-01-25 00:22:20 +0300

    fix: CostItem.js removed the training button

--- react-cost-counter/src/components/Costs/CostItem.js
+++ react-cost-counter/src/components/Costs/CostItem.js
@@ -1,28 +1,19 @@
 import './CostItem.css';
 import CostDate from './CostDate';
 import Card from '../UI/Card';
-import React, { useState } from 'react';
 
 const CostItem = (props) => {
 
-    const [description, setDescription] = useState(props.description);
-
-    const changeDescriptionHandler = () => {
-        setDescription('New item');
-        console.log(description);
-    };
-   
     return (
         <Card className='cost-item'>
             <CostDate date= { props.date } />
         
             <div className='cost-item__description'>
-                <h2>{ description }</h2>
+                <h2>{ props.description }</h2>
                 <div className='cost-item__price'>
                     ${ props.amount }
                 </div>
             </div>
-            <button onClick={ changeDescriptionHandler }>Change description</button>
         </Card>
     );
 }
